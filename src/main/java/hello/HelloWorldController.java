bdff1ef00400e631239ee5df7f8de2868922f2e0
manjupriyabe93 <37923165+manjupriyabe93@users.noreply.github.com>
2018-03-30 11:27:29 +0530

    Update HelloWorldController.java

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -1,20 +1,51 @@
 package hello;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/webhook")
+@RestController
+@RequestMapping("/api")
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.POST)
-    public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
-
-        System.out.println(obj);
-
-        return new WebhookResponse("Hello! " + obj, "Text " + obj);
+	@RequestMapping(value="/getTotalOrders",method=RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getTotalOrders(@RequestBody String obj)
+	{
+       List<WebResponse> list = new ArrayList<>();
+		
+		WebResponse response = new WebResponse("CREATED","1536");  
+		WebResponse response1 = new WebResponse("IncompleteOrder","501");
+		WebResponse response2 = new WebResponse("FAILED","50");
+		WebResponse response3 = new WebResponse("PROCESSING","24130");
+		list.add(response);
+		list.add(response1);
+		list.add(response2);
+		list.add(response3);
+		return new ResponseEntity<>(list, HttpStatus.OK);
     }
+	
+	@RequestMapping(value="/groupByOrderChannel",method=RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> groupByOrderChannel(@RequestBody String obj)
+	{
+       List<WebResponse> list = new ArrayList<>();
+		
+		WebResponse response = new WebResponse("Mobile Browser","1536");  
+		WebResponse response1 = new WebResponse("App","11225");
+		WebResponse response2 = new WebResponse("Desktop","19145");
+		WebResponse response3 = new WebResponse("Browser","19641");
+		list.add(response);
+		list.add(response1);
+		list.add(response2);
+		list.add(response3);
+		return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+    
+    
 }
